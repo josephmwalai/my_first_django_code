08e05c885ebed141a03532251ac76c6933d3ae14
joseph mwalai <josephmwalai@gmail.com>
2024-10-11 09:09:21 +0300

    urls changes

--- comrades/urls.py
+++ comrades/urls.py
@@ -3,6 +3,6 @@ from comrades.views import ComradesListView, ComradesDetailView, ComradesCreateV
 
 urlpatterns = [
     path('comradesList', ComradesListView.as_view()),
-    path('comradesDetails', ComradesDetailView.as_view()),
+    path('comradesDetails/<str:email>/', ComradesDetailView.as_view()),
     path('createComrade', ComradesCreateView.as_view())
 ]
